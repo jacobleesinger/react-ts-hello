68c7b47e69e3c31e179d52e7c61b9150459a2981
Jacob Singer <jacobleesinger@gmail.com>
2024-04-11 10:32:44 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import helloThere from './assets/Hello_there.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <img src={helloThere} alt='Hello there' />
+      <p>(the angel from my nightmare)</p>
+    </>
+  );
+}
+
+export default App;
